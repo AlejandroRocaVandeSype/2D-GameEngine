45e4c5dedb446c7a4d469661c56abb7fa9006f52
Alejandro Roca Vande Sype <roca-vandesype@hotmail.com>
2023-05-02 19:36:37 +0200

    MoveComponent with boundaries and PlayerInputCP

--- Galaga/MoveCommand.cpp
+++ Galaga/MoveCommand.cpp
@@ -1,30 +1,25 @@
 #include "MoveCommand.h"
 #include "GameObject.h"
+#include "MoveComponent.h"
 #include <iostream>
 
 
 
-MoveCommand::MoveCommand(engine::GameObject* actor, glm::vec3 direction, float speed)
+MoveCommand::MoveCommand(engine::GameObject* actor, glm::vec3 direction)
 	: Command()
 	, m_Actor(actor)
-	, m_Speed{ speed }	
 {
 	m_Direction = glm::normalize(direction);   // Normalized vector with the same direction but with lenght = 1
 }
 
 void MoveCommand::Execute(float deltaTime)
 {
-	auto transformCP = m_Actor->GetComponent<engine::TransformComponent>();
-	
-	if (transformCP)
+	auto moveCP = m_Actor->GetComponent<MoveComponent>();
+
+	if (moveCP != nullptr)
 	{
-		glm::vec3 pos = transformCP->GetLocalPosition();
-
-		pos += m_Direction * m_Speed * deltaTime;
-
-		transformCP->SetLocalPosition(pos);
+		moveCP->Move(deltaTime, m_Direction);
 	}
-	
 }
 
 
