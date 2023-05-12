ab129c5f493ecb0f2aa10d7524f8b32554b6b72e
Alejandro Roca Vande Sype <roca-vandesype@hotmail.com>
2023-05-12 11:24:36 +0200

    Service alocator and sound system as a service

---
+++ Minigin/Null_Sound_System.h
@@ -0,0 +1,19 @@
+#ifndef GAME_ENGINE_NULL_SOUND_SYSTEM
+#define GAME_ENGINE_NULL_SOUND_SYSTEM
+
+#include "SoundSystem.h"
+
+namespace engine
+{
+	// Class that doesnt do anything. It is used as a Null provider when no sound system has 
+	// been provided to the service alocator
+	class Null_Sound_System final : public engine::SoundSystem
+	{
+	public:
+		void PlaySound([[maybe_unused]] const sound_id, [[maybe_unused]] const float volume) override {};
+	};
+
+}
+
+#endif
+
