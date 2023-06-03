8c99368edef4e17ae8f918e5093687815efc00c6
Alejandro Roca Vande Sype <roca-vandesype@hotmail.com>
2023-06-03 19:20:45 +0200

    State pattern applied with the Game State

--- Galaga/FormationState.cpp
+++
@@ -1,6 +0,0 @@
-#include "FormationState.h"
-#include "SpawningState.h"
-#include "SendingEnemiesState.h"
-
-SpawningState* FormationState::spawning = new SpawningState();
-SendingEnemiesState* FormationState::sendingEnemies = new SendingEnemiesState();
