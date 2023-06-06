d5b4f15b0c2288ed6cb212f1621b96bdfe027ec7
Alejandro Roca Vande Sype <roca-vandesype@hotmail.com>
2023-06-06 11:30:51 +0200

    Menu navigation with controller

--- Galaga/PlayerInputCP.h
+++ Galaga/PlayerInputCP.h
@@ -3,6 +3,7 @@
 
 #include "Component.h"
 
+class MenuSelectionCP;
 // SETUP ALL INPUT NEEDED IN ORDER TO CONTROL THE PLAYER CHARACTER
 class PlayerInputCP final : public engine::Component
 {
@@ -13,11 +14,16 @@ public:
 	virtual void Update(const float deltaTime) override;
 	virtual void ReceiveMessage(const std::string& message, const std::string& value) override;
 
-	void AddControllerMovement();
+	void AddMenuInput(MenuSelectionCP* pMenuSelectionCP);
+	void AddGameplayInput();
+	void AddControllerInput(MenuSelectionCP* pMenuSelectionCP = nullptr);
+
+	//void AddControllerMovement();
 
 	void SetPlayerDied();
 
 private:
+
 	bool m_PlayerDied;			// This is to ensure we unbind commands associated to the gameObject when it dies and not 
 								// when program closes
 	int m_ControllerIdx;		// Controller being used
